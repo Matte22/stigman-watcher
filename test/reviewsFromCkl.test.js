488db435dcd580e3096cb03da2f3054e5949f582
matte22 <mferreira1264@gmail.com>
2023-11-20 12:22:12 -0500

    add tests

--- test/reviewsFromCkl.test.js
+++ test/reviewsFromCkl.test.js
@@ -15,6 +15,56 @@ const valueProcessor = function (
   he.decode(tagValue);
 };
 
+// Create a helper function to read the file and generate the review object
+async function generateReviewObject(
+  filePath,
+  importOptions,
+  fieldSettings,
+  allowAccept
+) {
+  const data = await fs.readFile(filePath, "utf8");
+  return ReviewParser.reviewsFromCkl({
+    data,
+    importOptions,
+    fieldSettings,
+    allowAccept,
+    valueProcessor,
+    XMLParser,
+  });
+}
+const parseOptions = {
+  allowBooleanAttributes: false,
+  attributeNamePrefix: "",
+  cdataPropName: "__cdata", //default is 'false'
+  ignoreAttributes: false,
+  parseTagValue: false,
+  parseAttributeValue: false,
+  removeNSPrefix: true,
+  trimValues: true,
+  tagValueProcessor: valueProcessor,
+  commentPropName: "__comment",
+  isArray: (name, jpath, isLeafNode, isAttribute) => {
+    return name === "__comment" || !isLeafNode;
+  },
+};
+
+async function getTotalVulnCount(filePath) {
+  const data = await fs.readFile(filePath, "utf8");
+  const parser = new XMLParser(parseOptions);
+  const parsed = parser.parse(data);
+
+  let totalVulnCount = 0;
+  parsed.CHECKLIST.forEach((checklist) => {
+    checklist.STIGS.forEach((stigs) => {
+      stigs.iSTIG.forEach((stig) => {
+        totalVulnCount += stig.VULN.length;
+      });
+    });
+  });
+
+  return totalVulnCount;
+}
+
 describe("generateReview from CKL", () => {
   it("generates review object with default settings", async () => {
     const importOptions = {
@@ -41,7 +91,6 @@ describe("generateReview from CKL", () => {
 
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
-    const data = await fs.readFile(filePath, "utf8");
 
     // expected statuses for each rule
     const expectedStatuses = {
@@ -55,19 +104,19 @@ describe("generateReview from CKL", () => {
       "SV-207191r803418_rule": "saved",
     };
 
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
-    });
+      allowAccept
+    );
 
     // ensuring that each review has a status that matches the expected status
-    review.checklists[0].reviews.forEach((reviewItem) => {
-      const expectedStatus = expectedStatuses[reviewItem.ruleId];
-      expect(reviewItem.status).to.equal(expectedStatus);
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        const expectedStatus = expectedStatuses[reviewItem.ruleId];
+        expect(reviewItem.status).to.equal(expectedStatus);
+      });
     });
   });
 
@@ -97,20 +146,19 @@ describe("generateReview from CKL", () => {
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
 
-    const data = await fs.readFile(filePath, "utf8");
-
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
-    });
+      allowAccept
+    );
+
     // ensuring that each review does not have a status
 
-    review.checklists[0].reviews.forEach((reviewItem) => {
-      expect(reviewItem.status).to.not.exist;
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        expect(reviewItem.status).to.not.exist;
+      });
     });
   });
 
@@ -140,16 +188,12 @@ describe("generateReview from CKL", () => {
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
 
-    const data = await fs.readFile(filePath, "utf8");
-
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
-    });
+      allowAccept
+    );
 
     // expected statuses for each rule
     const expectedStatuses = {
@@ -164,10 +208,11 @@ describe("generateReview from CKL", () => {
     };
 
     // ensuring that each review has a status that matches the expected status
-
-    review.checklists[0].reviews.forEach((reviewItem) => {
-      const expectedStatus = expectedStatuses[reviewItem.ruleId];
-      expect(reviewItem.status).to.equal(expectedStatus);
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        const expectedStatus = expectedStatuses[reviewItem.ruleId];
+        expect(reviewItem.status).to.equal(expectedStatus);
+      });
     });
   });
 
@@ -195,16 +240,13 @@ describe("generateReview from CKL", () => {
     const allowAccept = true;
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
-    const data = await fs.readFile(filePath, "utf8");
 
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
-    });
+      allowAccept
+    );
     // expected statuses for each rule
     const expectedStatuses = {
       "SV-207184r695317_rule": "accepted",
@@ -224,7 +266,7 @@ describe("generateReview from CKL", () => {
     });
   });
 
-  it("generates review object with 'unreviewed = commented' to simulate include unrevieweed rules: Having Comments ", async () => {
+  it("generates review object with 'unreviewed = commented' to simulate include unreviewed rules: Having Comments ", async () => {
     const importOptions = {
       autoStatus: "saved",
       unreviewed: "commented",
@@ -250,24 +292,22 @@ describe("generateReview from CKL", () => {
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
 
-    const data = await fs.readFile(filePath, "utf8");
-
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
-    });
+      allowAccept
+    );
 
-    review.checklists[0].reviews.forEach((reviewItem) => {
-      // expcet that each review has a commment value or detail value that != null
-      expect(reviewItem.detail || reviewItem.comment).to.not.be.null;
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        // expcet that each review has a commment value or detail value that != null
+        expect(reviewItem.detail || reviewItem.comment).to.not.be.null;
+      });
     });
   });
 
-  it("generates review object with 'unreviewed = always' to simulate include unrevieweed rules: Always ", async () => {
+  it("generates review object with 'unreviewed = always' to simulate include unreviewed rules: Always ", async () => {
     const importOptions = {
       autoStatus: "saved",
       unreviewed: "always",
@@ -290,37 +330,113 @@ describe("generateReview from CKL", () => {
 
     const allowAccept = true;
 
+    // const multipleFilePaths =
+    //   "./WATCHER-test-files/WATCHER/Asset_a-multi-stig.ckl";
+
     const filePath =
       "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
 
-    const data = await fs.readFile(filePath, "utf8");
+    const totalReviewsPreProcessed = await getTotalVulnCount(filePath);
 
-    const review = ReviewParser.reviewsFromCkl({
-      data,
+    const review = await generateReviewObject(
+      filePath,
       importOptions,
       fieldSettings,
-      allowAccept,
-      valueProcessor,
-      XMLParser,
+      allowAccept
+    );
+
+    const totalReviewsInOutput = review.checklists.reduce(
+      (acc, checklist) => acc + checklist.reviews.length,
+      0
+    );
+
+    console.log(JSON.stringify(review, null, 2));
+
+    // Assert that the total counts match
+    expect(totalReviewsInOutput).to.equal(
+      totalReviewsPreProcessed,
+      "The total number of reviews should match the total VULN items in source data"
+    );
+
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        const hasComments = !!reviewItem.comment || !!reviewItem.detail;
+        if (reviewItem.status === "notchecked") {
+          if (hasComments) {
+            expect(reviewItem.status).to.equal(
+              importOptions.unreviewedCommented
+            );
+          } else {
+            expect(reviewItem.status).to.equal("notchecked");
+          }
+        }
+      });
     });
+  });
+  it("generates review object with 'unreviewed = never' to simulate include unreviewed rules: Never ", async () => {
+    const importOptions = {
+      autoStatus: "saved",
+      unreviewed: "never",
+      unreviewedCommented: "informational",
+      emptyDetail: "replace",
+      emptyComment: "ignore",
+      allowCustom: true,
+    };
 
-    //console.log(JSON.stringify(review, null, 2));
+    const fieldSettings = {
+      detail: {
+        enabled: "always",
+        required: "always",
+      },
+      comment: {
+        enabled: "findings",
+        required: "findings",
+      },
+    };
 
-    // const expectedNumberOfReviews = 8;
-    // const reviews = review.checklists[0].reviews;
-    // const filteredReviews = reviews.filter((r) => r.detail || r.comment);
+    const allowAccept = true;
 
-    // expect(filteredReviews).to.have.lengthOf(expectedNumberOfReviews);
+    // const multipleFilePaths =
+    //   "./WATCHER-test-files/WATCHER/Asset_a-multi-stig.ckl";
 
-    // filteredReviews.forEach((reviewItem) => {
-    //   expect(reviewItem).to.have.all.keys(
-    //     "ruleId",
-    //     "result",
-    //     "detail",
-    //     "comment",
-    //     "resultEngine",
-    //     "status"
-    //   );
-    // });
+    const filePath =
+      "./WATCHER-test-files/WATCHER/Asset_a-VPN_TRUNCATED-V2R5.ckl";
+
+    const totalReviewsPreProcessed = await getTotalVulnCount(filePath);
+
+    const review = await generateReviewObject(
+      filePath,
+      importOptions,
+      fieldSettings,
+      allowAccept
+    );
+
+    const totalReviewsInOutput = review.checklists.reduce(
+      (acc, checklist) => acc + checklist.reviews.length,
+      0
+    );
+
+    console.log(JSON.stringify(review, null, 2));
+
+    // Assert that the total counts match
+    expect(totalReviewsInOutput).to.equal(
+      totalReviewsPreProcessed,
+      "The total number of reviews should match the total VULN items in source data"
+    );
+
+    review.checklists.forEach((checklist) => {
+      checklist.reviews.forEach((reviewItem) => {
+        const hasComments = !!reviewItem.comment || !!reviewItem.detail;
+        if (reviewItem.status === "notchecked") {
+          if (hasComments) {
+            expect(reviewItem.status).to.equal(
+              importOptions.unreviewedCommented
+            );
+          } else {
+            expect(reviewItem.status).to.equal("notchecked");
+          }
+        }
+      });
+    });
   });
 });
